e20b988f1fc96e91ade8f30f1735483061431c90
pristinsky1 <44822448+pristinsky1@users.noreply.github.com>
2020-11-12 12:14:15 -0800

    Update data.py

--- src/data.py
+++ src/data.py
@@ -1,4 +1,37 @@
 import pandas as pd
+import os
+
+
+def get_data(indir, outdir):
+    '''
+    Reads the data by creating a symlink between the 
+    location of the downloaded data and /data
+    '''
+    # first create the data directory
+    directory = "data"
+    parent_dir = "./"
+    path = os.path.join(parent_dir, directory)
+
+    os.mkdir(path)
+
+    # create a convenient hierarchical structure of folders inside /data
+    directory1 = "raw"
+    directory2 = "temp"
+    directory3 = "out"
+    parent_dir = "./data/"
+    
+    os.mkdir(os.path.join(parent_dir, directory1))
+    os.mkdir(os.path.join(parent_dir, directory2))
+    os.mkdir(os.path.join(parent_dir, directory3))
+    
+    # create the symlink
+    os.symlink(indir, outdir) 
+        
+    return pd.read_csv(outdir)
+
+
+
+
 
 def modify_data(raw_data):
     #we are separating the column "packet sizes"
