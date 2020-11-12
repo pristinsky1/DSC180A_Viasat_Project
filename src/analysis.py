81e51847a660dd8ba69c4841e34cfbd3996d63f3
pristinsky1 <44822448+pristinsky1@users.noreply.github.com>
2020-11-12 00:22:48 -0800

    Update analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Example of byte count feature extraction on the no streaming dataset:
+big_byte_no_streaming = big_byte_count_feature(yes_0)
+print(big_byte_no_streaming[['Dir1_ByteCount_0to300', 'Dir2_ByteCount_1200to1500']].head(5))
+print("No Streaming Byte Count:")
+big_byte_no_streaming[['Dir1_ByteCount_0to300', 'Dir2_ByteCount_1200to1500']].sum()
 
