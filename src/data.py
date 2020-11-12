691930f1714c819b923cef5d10957647ea6c0c3f
Arely Vasquez <47491918+arv020@users.noreply.github.com>
2020-11-12 12:07:37 -0800

    Update data.py

--- src/data.py
+++ src/data.py
@@ -1,14 +1,5 @@
 import pandas as pd
 
-# This function returns a dataframe with the packets times, sizes, and directions for a single row of data.
-# This will be used within our other functions to help create the features.
-def three_cols(row):
-    time = list(map(int, row['packet_times'].split(';')[0:-1]))
-    size = list(map(int, row['packet_sizes'].split(';')[0:-1]))
-    dirs = list(map(int, row['packet_dirs'].split(';')[0:-1]))
-    dict1 = {'packet_time': time, 'packet_size': size, 'packet_dir': dirs}
-    return pd.DataFrame(dict1)
-
 def modify_data(raw_data):
     #we are separating the column "packet sizes"
     new_pksize = []
