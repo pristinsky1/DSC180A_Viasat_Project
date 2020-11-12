0c0a0086eb590d2c71103ae01149e4ac7be66a68
pristinsky1 <44822448+pristinsky1@users.noreply.github.com>
2020-11-12 11:45:49 -0800

    Update features.py

--- src/features.py
+++ src/features.py
@@ -22,3 +22,11 @@ def big_byte_count_feature(dataset):
         packet_size_count1.append(one_count)
         packet_size_count2.append(two_count)
     return df.assign(Dir1_ByteCount_0to300 = packet_size_count1, Dir2_ByteCount_1200to1500 = packet_size_count2)
+
+
+def binarymean_packetsizes(modify_data(raw_data), mean_num):
+    return (modified_data(raw_data)["packet_sizes"] > mean_num).replace({True: 1, False: 0})
+  
+  
+  def binarymin_packetsizes(modify_data(raw_data), min_num):
+    return (modify_data(raw_data)["packet_sizes"].min() <= min_num)
