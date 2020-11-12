30c0786e86dd10094d23a61053a3594ecd9d2cc3
Arely Vasquez <47491918+arv020@users.noreply.github.com>
2020-11-12 11:45:04 -0800

    Update analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -15,3 +15,20 @@ def streaming_viz(dataset):
   print(big_byte_streaming[['Dir1_ByteCount_0to300', 'Dir2_ByteCount_1200to1500']].head(5))
   print("Streaming Byte Count:")
   return big_byte_streaming[['Dir1_ByteCount_0to300', 'Dir2_ByteCount_1200to1500']].sum()
+
+
+def pktdir_vs_pktsze_int(modified_data):
+  modified_data[modified_data["packet_dir"] == 1]["packet_sizes"].plot(kind = "hist")
+  modified_data[modified_data["packet_dir"] == 2]["packet_sizes"].plot(kind = "hist")
+  pl.title('Packet Directions vs Packet Size: internet browsing data with VPN')
+  pl.xlabel('Packet Size')
+  pl.ylabel('Frequency')
+  pl.legend(["1->2", "2->1"])
+
+def pktdir_vs_pktsze_vid(modified_data):
+  modified_data[modified_data["packet_dir"] == 1]["packet_sizes"].plot(kind = "hist")
+  modified_data[modified_data["packet_dir"] == 2]["packet_sizes"].plot(kind = "hist")
+  pl.title('Packet Directions vs Packet Size: video streaming data with VPN')
+  pl.xlabel('Packet Size')
+  pl.ylabel('Frequency')
+  pl.legend(["1->2", "2->1"])
