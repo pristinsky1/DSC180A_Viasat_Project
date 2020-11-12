ffe597673750ab62a91f735fa7f6407e0b6d7698
Arely Vasquez <47491918+arv020@users.noreply.github.com>
2020-11-11 22:31:12 -0800

    Update data2.py

--- src/data2.py
+++ src/data2.py
@@ -22,18 +22,3 @@ def modify_data(raw_data):
     return modified_data
 
   
-def pktdir_vs_pktsze_int(modified_data):
-  modified_data[modified_data["packet_dir"] == 1]["packet_sizes"].plot(kind = "hist")
-  modified_data[modified_data["packet_dir"] == 2]["packet_sizes"].plot(kind = "hist")
-  pl.title('Packet Directions vs Packet Size: internet browsing data with VPN')
-  pl.xlabel('Packet Size')
-  pl.ylabel('Frequency')
-  pl.legend(["1->2", "2->1"])
-
-def pktdir_vs_pktsze_vid(modified_data):
-  modified_data[modified_data["packet_dir"] == 1]["packet_sizes"].plot(kind = "hist")
-  modified_data[modified_data["packet_dir"] == 2]["packet_sizes"].plot(kind = "hist")
-  pl.title('Packet Directions vs Packet Size: video streaming data with VPN')
-  pl.xlabel('Packet Size')
-  pl.ylabel('Frequency')
-  pl.legend(["1->2", "2->1"])
