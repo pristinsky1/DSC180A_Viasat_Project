d409c7392d1b2d7f77f922768280ac8b5fd67701
pristinsky1 <44822448+pristinsky1@users.noreply.github.com>
2020-11-12 00:56:02 -0800

    Update features.py

--- src/features.py
+++ src/features.py
@@ -1,15 +1,6 @@
 import pandas as pd
 import numpy as np
 
-# This function returns a dataframe with the packets times, sizes, and directions for a single row of data.
-# This will be used within our other functions to help create the features.
-def three_cols(row):
-    time = list(map(int, row['packet_times'].split(';')[0:-1]))
-    size = list(map(int, row['packet_sizes'].split(';')[0:-1]))
-    dirs = list(map(int, row['packet_dirs'].split(';')[0:-1]))
-    dict1 = {'packet_time': time, 'packet_size': size, 'packet_dir': dirs}
-    return pd.DataFrame(dict1)
-
 # This function takes all the counts of the 0-300bytes for the 1->2 Direction and all the counts
 # of the 1200-1500bytes for the 2->1 Direction and creates new columns based off this in the dataframe
 def big_byte_count_feature(dataset):        
