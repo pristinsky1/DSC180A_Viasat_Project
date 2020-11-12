2b87973208ca38c6b549f4b7e78a486a4b9dcab0
Arely Vasquez <47491918+arv020@users.noreply.github.com>
2020-11-11 22:28:03 -0800

    Update features2.py

--- src/features2.py
+++ src/features2.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import numpy as np
+import pylab as pl
+
+def binarymean_packetsizes(modify_data(raw_data)):
+    return (modified_data(raw_data)["packet_sizes"] > 500).replace({True: 1, False: 0})
+  
+  
+  def binarymin_packetsizes(modify_data(raw_data)):
+    return (modify_data(raw_data)["packet_sizes"].min() <= 32)
+
 
