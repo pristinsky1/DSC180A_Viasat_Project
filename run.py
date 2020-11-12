8de7c2ace1475ff5fde378314223ea987edaca51
Arely Vasquez <47491918+arv020@users.noreply.github.com>
2020-11-12 12:44:51 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,42 @@
+
+import sys
 import json
+import pandas as pd
+
+sys.path.insert(0, 'src')
+from data import get_data modify_data
+from analysis import no_streaming_viz streaming_viz pktdir_vs_pktsze_int pktdir_vs_pktsze_vid
+from features import big_byte_count_feature binarymean_packetsizes binarymin_packetsizes
+from utils import convert_notebook
+
+
+
+def main(targets):
+
+    data_config = json.load(open('config/data-params.json'))
+    analysis_config = json.load(open('config/analysis-params.json'))
+    features_config = json.load(open('config/features-params.json'))
+    
+
+    if 'data' in targets:
+
+        data = generate_data(**data_config)
+        save_data(data, **data_config)
+
+    if 'eda' in targets:
+
+        try:
+            data
+        except NameError:
+            data = pd.read_csv(data_config['data_fp'])
+
+        generate_stats(data, **eda_config)
+        
+        # execute notebook / convert to html
+        convert_notebook(**eda_config)
+
+
+if __name__ == '__main__':
+
+    targets = sys.argv[1:]
+    main(targets)
