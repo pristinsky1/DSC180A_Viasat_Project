4b9aaaf3e36fd0901621a5e6dac934024542c81a
Arely Vasquez <47491918+arv020@users.noreply.github.com>
2020-11-12 11:46:10 -0800

    Update data.py

--- src/data.py
+++ src/data.py
@@ -8,3 +8,21 @@ def three_cols(row):
     dirs = list(map(int, row['packet_dirs'].split(';')[0:-1]))
     dict1 = {'packet_time': time, 'packet_size': size, 'packet_dir': dirs}
     return pd.DataFrame(dict1)
+
+def modify_data(raw_data):
+    #we are separating the column "packet sizes"
+    new_pksize = []
+    for i in raw_data["packet_sizes"]:
+        arr = i.split(";")
+        for j in arr[:-1]:
+            new_pksize.append(j)
+
+    #we are separating the column "packet dir"
+    new_pkdir = []
+    for i in raw_data["packet_dirs"]:
+        arr = i.split(";")
+        for j in arr[:-1]:
+            new_pkdir.append(j)
+
+    modified_data = pd.DataFrame({'packet_sizes': pd.to_numeric(new_pksize), 'packet_dir': pd.to_numeric(new_pkdir)})
+    return modified_data
