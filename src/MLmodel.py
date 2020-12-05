87556b0343b05875223bc044897e4b00e9ee8b46
pristinsky1 <44822448+pristinsky1@users.noreply.github.com>
2020-12-04 23:42:38 -0800

    Update MLmodel.py

--- src/MLmodel.py
+++ src/MLmodel.py
@@ -1,1 +1,29 @@
+import pandas as pd
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
+#Trains, tests, and splits the data up so that RandomForestClassifier can be used 
+#to train on the data and then determine how accurate the model is
+def ml_model_analysis(X, y):
+    model = RandomForestClassifier()
+    X_tr, X_ts, y_tr, y_ts = train_test_split(X, y, test_size=0.25, random_state=42)
+    model = model.fit(X_tr[['Dir1_ByteCount_0to300_feature','Dir2_ByteCount_1200to1500_feature']],y_tr)
+    prediction_test = model.predict(X_ts[['Dir1_ByteCount_0to300_feature','Dir2_ByteCount_1200to1500_feature']])
+    prediction_train = model.predict(X_tr[['Dir1_ByteCount_0to300_feature','Dir2_ByteCount_1200to1500_feature']])
+    print (("Base test accuracy", metrics.accuracy_score(y_ts, prediction_test)), 
+            ("Base Train Accuracy", metrics.accuracy_score(y_tr, prediction_train)))
+    return prediction_test, y_ts
+
+  
+#Trains the model on all the data found within the GoodData on dsmlp, and then predicts 
+#whether streaming or not for the input data chunk entered
+def ml_model_train(X, y, input_X, input_y):
+    model = RandomForestClassifier()
+    model = model.fit(X[['Dir1_ByteCount_0to300_feature','Dir2_ByteCount_1200to1500_feature']],y)
+    prediction = model.predict(input_X[['Dir1_ByteCount_0to300_feature','Dir2_ByteCount_1200to1500_feature']])
+    if prediction == 1:
+        val = True
+    else:
+        val = False
+    return ("Prediction Value: " + str(prediction[0]), "True Value: " + str(input_y[0]), 'is_streaming? : ' + str(val))
