fdb59ecdd7886642934c1f61e29b0829784d882b
pristinsky1 <44822448+pristinsky1@users.noreply.github.com>
2020-12-04 23:17:10 -0800

    Update features.py

--- src/features.py
+++ src/features.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import os
 
 # This function returns a dataframe with the packets times, sizes, and directions for a single row of data.
 # This will be used within our other functions to help create the features.
@@ -11,13 +12,13 @@ def three_cols(row):
     return pd.DataFrame(dict1)
 
 # This function takes all the counts of the 0-300bytes for the 1->2 Direction and all the counts
-# of the 1200-1500bytes for the 2->1 Direction and creates new columns based off this in the dataframe
+# of the 1200-1500bytes for the 2->1 Direction and creates sum values for the two features per dataset.
+# uses the three_cols function as a helper function
 def big_byte_count_feature(dataset):        
-    df = dataset.copy()
     packet_size_count1 = []
     packet_size_count2 = []
-    for i in range(df.shape[0]):
-        row = three_cols(df.iloc[i])
+    for i in range(dataset.shape[0]):
+        row = three_cols(dataset.iloc[i])
         ones = row.loc[row['packet_dir'] == 1]['packet_size']
         twos = row.loc[row['packet_dir'] == 2]['packet_size']
         one_count=0
@@ -30,8 +31,63 @@ def big_byte_count_feature(dataset):
                 two_count += 1
         packet_size_count1.append(one_count)
         packet_size_count2.append(two_count)
-    return df.assign(Dir1_ByteCount_0to300 = packet_size_count1, Dir2_ByteCount_1200to1500 = packet_size_count2)
+    return [sum(packet_size_count1), sum(packet_size_count2)]
+  
+  
+  # input: filepaths
+# output: 4 lists -> associated file names, labels, feature1, feature2
+# uses the big_byte_count_feature as a helper function
+def features_labels(filepath):
+    Dir1_ByteCount_0to300_feature = []
+    Dir2_ByteCount_1200to1500_feature = []
+    labels = []
+    file_names = []
+    files = os.listdir(filepath)
+    for file in files:
+        if ('novpn' in file) or (file[:2] == '._'):
+            continue
+        if 'novideo' in file:
+            labels.append(0)
+        else:
+            labels.append(1)
+        file_names.append(file)
+        df = pd.read_csv(filepath + '/' + file)
+        sum_values = big_byte_count_feature(df)
+        Dir1_ByteCount_0to300_feature.append(sum_values[0])
+        Dir2_ByteCount_1200to1500_feature.append(sum_values[1])
+    feature_df = pd.DataFrame(data={'Dir1_ByteCount_0to300_feature': Dir1_ByteCount_0to300_feature,
+                                    'Dir2_ByteCount_1200to1500_feature': Dir2_ByteCount_1200to1500_feature})
+    return file_names, labels, feature_df 
 
+# accesses the data file found within the data folder and creates the features and label for it
+# uses the big_byte_count_feature as a helper function
+def input_feature_label(filepath):
+    Dir1_ByteCount_0to300_feature = []
+    Dir2_ByteCount_1200to1500_feature = []
+    labels = []
+    file_names = []
+    files = os.listdir(filepath)
+    for file in files:
+        if ('novpn' in file) or (file[:2] == '._'):
+            return "File Invalid. Must be vpn data, nor can it be empty."
+        if 'novideo' in file:
+            labels.append(0)
+        else:
+            labels.append(1)
+        file_names.append(file)
+        df = pd.read_csv(filepath + file)
+        sum_values = big_byte_count_feature(df)
+        Dir1_ByteCount_0to300_feature.append(sum_values[0])
+        Dir2_ByteCount_1200to1500_feature.append(sum_values[1])
+    feature_df = pd.DataFrame(data={'Dir1_ByteCount_0to300_feature': Dir1_ByteCount_0to300_feature,
+                                    'Dir2_ByteCount_1200to1500_feature': Dir2_ByteCount_1200to1500_feature})
+    return file_names, labels, feature_df
+
+
+
+
+
+# Functions Arely Created --> Plz comment these once you get the chance
 def binarymean_packetsizes(modify_data(raw_data), mean_num):
     return (modified_data(raw_data)["packet_sizes"] > mean_num).replace({True: 1, False: 0})
   
