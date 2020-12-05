fbc9d32615a4d3bf5ab44e6d3253e336bb1943e1
pristinsky1 <44822448+pristinsky1@users.noreply.github.com>
2020-12-04 23:55:47 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,57 +1,48 @@
 
 import sys
 import json
+import os
 import pandas as pd
 
 sys.path.insert(0, 'src')
 from data import get_data modify_data
 from analysis import no_streaming_viz streaming_viz pktdir_vs_pktsze_int pktdir_vs_pktsze_vid
-from features import big_byte_count_feature binarymean_packetsizes binarymin_packetsizes
 from utils import convert_notebook
-
-
+from features import features_labels input_feature_label binarymean_packetsizes binarymin_packetsizes
+from all  import ml_model_analysis
+from all import ml_model_train
 
 def main(targets):
-
-    data_config = json.load(open('config/data-params.json'))
-    analysis_config = json.load(open('config/analysis-params.json'))
-    features_config = json.load(open('config/features-params.json'))
+    '''
+    Given the targets, main runs the main project pipeline logic.
+    '''
     
+    #grabs the parameters from the data config file
+    feature_cfg = json.load(open('config/data-params.json'))
+    
+    
+    if 'test' in targets:
+        file_names, file_labels, new_df = features_labels(feature_cfg['input_path'])
+        new_df.to_csv(feature_cfg['test_out_path'])
+        print("The associated test file names are: ") 
+        print(file_names)
+        print("The associated test file labels are: ") 
+        print(file_labels)
+        print("Created the new test features! Check folder test/output/ and observe the output features csv file!")
 
-    if 'data' in targets:
-        
-        # create the symlink
-   
-        link = os.symlink(data_config['raw_data'], outdir, target_is_directory = True)
-        data = get_data(link)
-        data = modify_data(data)
-
-        
-        #data = generate_data(**data_config)
-        #save_data(data, **data_config)
+    if 'result' in targets:
+        data_names, data_labels, data_df = features_labels(feature_cfg['train_path'])
+        input_names, input_labels, input_df = input_feature_label(feature_cfg['input_path'])
+        final_result = ml_model_train(data_df, data_labels, input_df, input_labels)
+        print(final_result)
 
     if 'analysis' in targets:
+        data_names, data_labels, data_df = features_labels(feature_cfg['train_path'])
+        prediction_labels, test_labels = ml_model_analysis(data_df, data_labels)
         
-        findings = analysis_config['dataset']
-        
-        
+    return
 
-#         try:
-#             data
-#         except NameError:
-#             data = pd.read_csv(data_config['raw_data'])
-
-#         generate_stats(data, **eda_config)
-        
-#         # execute notebook / convert to html
-#         convert_notebook(**eda_config)
-        
-     if 'features' in targets:
-            
-            new_features = features_config["mean_num"]
-
-
-if __name__ == '__main__':
-
+if __name__=='__main__':
+    #run via:
     targets = sys.argv[1:]
     main(targets)
