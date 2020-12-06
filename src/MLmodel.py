301e8a80b470815bb45de8d9344df3a1552a4546
pristinsky1 <44822448+pristinsky1@users.noreply.github.com>
2020-12-05 17:54:48 -0800

    Update MLmodel.py

--- src/MLmodel.py
+++ src/MLmodel.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+import pickle
 
 #Trains, tests, and splits the data up so that RandomForestClassifier can be used 
 #to train on the data and then determine how accurate the model is
@@ -18,12 +19,20 @@ def ml_model_analysis(X, y):
   
 #Trains the model on all the data found within the GoodData on dsmlp, and then predicts 
 #whether streaming or not for the input data chunk entered
-def ml_model_train(X, y, input_X, input_y):
+def ml_model_train(X, y, input_X, input_y, filename):
     model = RandomForestClassifier()
     model = model.fit(X[['Dir1_ByteCount_0to300_feature','Dir2_ByteCount_1200to1500_feature']],y)
-    prediction = model.predict(input_X[['Dir1_ByteCount_0to300_feature','Dir2_ByteCount_1200to1500_feature']])
-    if prediction == 1:
-        val = True
-    else:
-        val = False
-    return ("Prediction Value: " + str(prediction[0]), "True Value: " + str(input_y[0]), 'is_streaming? : ' + str(val))
+    # save the model to temp/model folder
+    pickle.dump(model, open(filename, 'wb'))
+    return
+
+def classifer(input_model, filename):
+    loaded_model = pickle.load(open(filename, 'rb'))
+    prediction = loaded_model.predict(input_X[['Dir1_ByteCount_0to300_feature','Dir2_ByteCount_1200to1500_feature']])
+    for i in range(0, len(prediction)):
+        if bool(prediction[i]) == bool(input_y[i]):
+            val = "Yes"
+        else:
+            val = "No"
+        print("is_streaming? Prediction Value: " + str(bool(prediction[i])), "is_streaming? True Value: " + str(bool(input_y[i])), "classified correctly? : " + val)
+    return
