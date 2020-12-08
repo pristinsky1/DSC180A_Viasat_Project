087dad81ac3ab1b2e43bd791a0b4fb421200c3aa
pristinsky1 <44822448+pristinsky1@users.noreply.github.com>
2020-12-08 15:42:50 -0800

    Update features.py

--- src/features.py
+++ src/features.py
@@ -40,10 +40,12 @@ def big_byte_count_feature(dataset):
 def features_labels(input_filepath, output_filepath):
     Dir1_ByteCount_0to300_feature = []
     Dir2_ByteCount_1200to1500_feature = []
+    max_prominence_feature = []
     labels = []
     file_names = []
     files = os.listdir(input_filepath)
     for file in files:
+        #eliminates files that don't match the structure of the datasets we are working with
         if ('novpn' in file) or (file[:2] == '._'):
             continue
         if 'novideo' in file:
@@ -52,11 +54,29 @@ def features_labels(input_filepath, output_filepath):
             labels.append(1)
         file_names.append(file)
         df = pd.read_csv(input_filepath + '/' + file)
+        #checks to see if file is empty, then eliminates it if empty
+        if len(df) == 0:
+            labels.pop()
+            file_names.pop()
+            continue
+        #this section creates the two bytecount features
         sum_values = big_byte_count_feature(df)
+        #appends the created values to feature list
         Dir1_ByteCount_0to300_feature.append(sum_values[0])
         Dir2_ByteCount_1200to1500_feature.append(sum_values[1])
+        #this section creates the max_prominence_feature value in a frequency domain for each dataset
+        df_temp = df[['Time', '2->1Bytes']].set_index('Time')
+        df_temp.index = pd.to_datetime(df_temp.index,unit='s')
+        df_temp = df_temp.resample('500ms').sum()
+        s = df_temp['2->1Bytes']
+        fs = 2
+        f, Pxx_den = signal.welch(s, fs, nperseg=len(s))
+        peaks, properties = find_peaks(np.sqrt(Pxx_den), prominence=1000)
+        max_prominence = properties['prominences'].max()
+        #appends the created value to feature list
+        max_prominence_feature.append(max_prominence)
     feature_label_df = pd.DataFrame(data={'data_file_name': file_names,'labels': labels, 'Dir1_ByteCount_0to300_feature': Dir1_ByteCount_0to300_feature,
-                                    'Dir2_ByteCount_1200to1500_feature': Dir2_ByteCount_1200to1500_feature})
+                                    'Dir2_ByteCount_1200to1500_feature': Dir2_ByteCount_1200to1500_feature, 'max_prominence_feature': max_prominence_feature})
     #look into using index=False, not sure if I need it here but could be something important
     feature_label_df.to_csv(path_or_buf=output_filepath)
     return feature_label_df
@@ -70,6 +90,7 @@ def input_feature_label(input_filepath, output_filepath):
     file_names = []
     files = os.listdir(input_filepath)
     for file in files:
+        #eliminates files that don't match the structure of the datasets we are working with
         if ('novpn' in file) or (file[:2] == '._'):
             return "File Invalid. Must be vpn data, nor can it be empty."
         if 'novideo' in file:
@@ -78,11 +99,29 @@ def input_feature_label(input_filepath, output_filepath):
             labels.append(1)
         file_names.append(file)
         df = pd.read_csv(input_filepath + file)
+        #checks to see if file is empty, then eliminates it if empty
+        if len(df) == 0:
+            labels.pop()
+            file_names.pop()
+            return "File is empty!"
+        #this section creates the two bytecount features
         sum_values = big_byte_count_feature(df)
+        #appends the created values to feature list
         Dir1_ByteCount_0to300_feature.append(sum_values[0])
         Dir2_ByteCount_1200to1500_feature.append(sum_values[1])
+        #this section creates the max_prominence_feature value in a frequency domain for each dataset
+        df_temp = df[['Time', '2->1Bytes']].set_index('Time')
+        df_temp.index = pd.to_datetime(df_temp.index,unit='s')
+        df_temp = df_temp.resample('500ms').sum()
+        s = df_temp['2->1Bytes']
+        fs = 2
+        f, Pxx_den = signal.welch(s, fs, nperseg=len(s))
+        peaks, properties = find_peaks(np.sqrt(Pxx_den), prominence=1000)
+        max_prominence = properties['prominences'].max()
+        #appends the created value to feature list
+        max_prominence_feature.append(max_prominence)
     feature_label_df = pd.DataFrame(data={'input_file_name': file_names,'labels': labels,'Dir1_ByteCount_0to300_feature': Dir1_ByteCount_0to300_feature,
-                                    'Dir2_ByteCount_1200to1500_feature': Dir2_ByteCount_1200to1500_feature})
+                                    'Dir2_ByteCount_1200to1500_feature': Dir2_ByteCount_1200to1500_feature, 'max_prominence_feature': max_prominence_feature})
     feature_label_df.to_csv(path_or_buf=output_filepath)
     return feature_label_df
 
