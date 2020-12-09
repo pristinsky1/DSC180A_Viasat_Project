2dfd65f6fdcaa36ce455d53339b316232d9133e4
Arely Vasquez <47491918+arv020@users.noreply.github.com>
2020-12-09 11:13:40 -0800

    Update data.py

--- src/data.py
+++ src/data.py
@@ -29,25 +29,3 @@ def create_temp_directory():
     os.mkdir(os.path.join(parent_dir, directory3))
         
     return
-
-
-
-
-
-def modify_data(raw_data):
-    #we are separating the column "packet sizes"
-    new_pksize = []
-    for i in raw_data["packet_sizes"]:
-        arr = i.split(";")
-        for j in arr[:-1]:
-            new_pksize.append(j)
-
-    #we are separating the column "packet dir"
-    new_pkdir = []
-    for i in raw_data["packet_dirs"]:
-        arr = i.split(";")
-        for j in arr[:-1]:
-            new_pkdir.append(j)
-
-    modified_data = pd.DataFrame({'packet_sizes': pd.to_numeric(new_pksize), 'packet_dir': pd.to_numeric(new_pkdir)})
-    return modified_data
