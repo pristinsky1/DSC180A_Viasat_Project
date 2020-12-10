58876289565a0a5f8e1071a1ac2444a930506375
pristinsky1 <44822448+pristinsky1@users.noreply.github.com>
2020-12-09 16:05:10 -0800

    Update run.py

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ from data import create_temp_directory
 from analysis import no_streaming_viz, streaming_viz, pktdir_vs_pktsze_int, pktdir_vs_pktsze_vid
 #from utils import convert_notebook
 from features import features_labels, input_feature_label
-from MLmodel import ml_model_analysis, ml_model_train, final_classifier
+from mLModel import ml_model_analysis, ml_model_train, final_classifier
 
 def main(targets):
     '''
