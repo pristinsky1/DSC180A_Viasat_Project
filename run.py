bef9a4514cb3c7131eb857112a92d4bb2d128423
pristinsky1 <44822448+pristinsky1@users.noreply.github.com>
2020-12-11 00:18:42 -0800

    Update run.py

--- run.py
+++ run.py
@@ -26,12 +26,20 @@ def main(targets):
     if 'test' in targets:
         #creates the local temp directory for intermediate steps to be placed
         create_temp_directory()
-        new_df = features_labels(data_cfg['test_path'], data_cfg['test_out_path'])
+        features_labels(data_cfg['test_path'], data_cfg['test_out_path'])
         print("The associated test file names are: ") 
         print(new_df['data_file_name'])
         print("The associated test file labels are: ") 
         print(new_df['labels'])
         print("Created the new test features! Check folder test/output/ and observe the output features csv file!")
+        
+        train_df = features_labels(data_cfg['train_path'], feature_cfg['feature_path'])
+        input_feature_df = input_feature_label(data_cfg['input_path'], feature_cfg['input_feature_path'])
+        ml_model_train(feature_cfg['feature_path'], model_cfg['trained_model'])
+        data_df = features_labels(data_cfg['train_path'], feature_cfg['feature_path'])
+        prediction_labels, test_labels = ml_model_analysis(data_df, data_df['labels'])
+        input_df = pd.read_csv(feature_cfg['input_feature_path'])
+        final_classifier(input_df, model_cfg['trained_model'])
     
     if 'features' in targets:
         #creates the local temp directory for intermediate steps to be placed
