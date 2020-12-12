4d97bca278deadab5269d3289e7d147dd993bb08
Arely Vasquez <47491918+arv020@users.noreply.github.com>
2020-12-11 20:22:01 -0800

    Update analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -7,16 +7,16 @@ from features import big_byte_count_feature
 def no_streaming_viz(dataset):
   #Example of byte count feature extraction on the no streaming dataset:
   big_byte_no_streaming = big_byte_count_feature(dataset)
-  #print(big_byte_no_streaming[['Dir1_ByteCount_0to300', 'Dir2_ByteCount_1200to1500']].head(5))
-  print("No Streaming Byte Count:")
-  return big_byte_no_streaming[['Dir1_ByteCount_0to300', 'Dir2_ByteCount_1200to1500']].sum()
+ 
+  print("No Streaming Byte Count: ")
+  return big_byte_no_streaming.sum()
 
 def streaming_viz(dataset):
   #Example of byte count feature extraction on the streaming dataset:
   big_byte_streaming = big_byte_count_feature(dataset)
-  #print(big_byte_streaming[['Dir1_ByteCount_0to300', 'Dir2_ByteCount_1200to1500']].head(5))
-  print("Streaming Byte Count:")
-  return big_byte_streaming[['Dir1_ByteCount_0to300', 'Dir2_ByteCount_1200to1500']].sum()
+ 
+  print("Streaming Byte Count: ")
+  return big_byte_streaming.sum()
 
 
 def pktdir_vs_pktsze_int(modified_data):
